7cb388ba347c1bcdf1211b01079b5600bee98798
Brandon <brandon.vanbellinghen@student.vinci.be>
2023-09-29 09:34:32 +0200

    exemple Theorie Module3

---
+++ Exemple/module3/partie 1/exemple2/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [value/*, setValue*/] = useState(10)
+
+  const handleClick = () =>
+    console.log('clicked the button')
+
+  return (
+    <div>
+      {value}
+      <button onClick={handleClick}>button</button>
+    </div>
+  )
+}
+
+export default App
